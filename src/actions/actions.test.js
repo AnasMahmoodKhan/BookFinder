22b4cd4ad2e06ef2aa13fbfba17a28e22530b42c
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-04-29 16:42:39 +0530

    Updated tests for actions.js

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -1,8 +1,20 @@
 import moxios from "moxios";
 import configureMockStore from "redux-mock-store";
 import thunk from "redux-thunk";
-import { getBooks, receiveBooks, requestBooks } from "./actions";
-import { RECEIVE_BOOKS, REQUEST_BOOKS } from "./actionTypes";
+import {
+  getBookInfo,
+  getBooks,
+  receiveBookInfo,
+  receiveBooks,
+  requestBookInfo,
+  requestBooks,
+} from "./actions";
+import {
+  RECEIVE_BOOKS,
+  RECEIVE_BOOK_INFO,
+  REQUEST_BOOKS,
+  REQUEST_BOOK_INFO,
+} from "./actionTypes";
 
 describe("test for actions dispatch", () => {
   let store;
@@ -36,6 +48,28 @@ describe("test for actions dispatch", () => {
       { type: RECEIVE_BOOKS, status: "error", payload: [] },
     ];
     store.dispatch(receiveBooks({ status: "error", payload: [] }));
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  test("for requestBookInfo action dispatch", () => {
+    const expectedActions = [{ type: REQUEST_BOOK_INFO, ID: "xiouia3" }];
+    store.dispatch(requestBookInfo("xiouia3"));
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  test("for receiveBookInfo `status = success` action dispatch", () => {
+    const expectedActions = [
+      { type: RECEIVE_BOOK_INFO, status: "success", payload: [] },
+    ];
+    store.dispatch(receiveBookInfo({ status: "success", payload: [] }));
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  test("for receiveBookInfo `status = error` action dispatch", () => {
+    const expectedActions = [
+      { type: RECEIVE_BOOK_INFO, status: "error", payload: [] },
+    ];
+    store.dispatch(receiveBookInfo({ status: "error", payload: [] }));
     expect(store.getActions()).toEqual(expectedActions);
   });
 
@@ -86,6 +120,58 @@ describe("test for actions dispatch", () => {
       ];
 
       return store.dispatch(getBooks("abc")).then(() => {
+        expect(store.getActions()).toEqual(expectedActions);
+      });
+    });
+  });
+
+  describe("test for getBookInfo api call", () => {
+    beforeEach(() => {
+      moxios.install();
+    });
+    afterEach(() => {
+      moxios.uninstall();
+    });
+
+    test("for getBooks api call when success", () => {
+      const data = [
+        {
+          name: "abc",
+          title: "xyz",
+        },
+      ];
+
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent();
+        request.respondWith({
+          status: 200,
+          response: data,
+        });
+      });
+      const expectedActions = [
+        { type: REQUEST_BOOK_INFO, ID: "xjlmkfs" },
+        { type: RECEIVE_BOOK_INFO, status: "success", payload: data },
+      ];
+
+      return store.dispatch(getBookInfo("xjlmkfs")).then(() => {
+        expect(store.getActions()).toEqual(expectedActions);
+      });
+    });
+
+    test("for getBooks api call when error", () => {
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent();
+        request.respondWith({
+          status: 400,
+          response: "error",
+        });
+      });
+      const expectedActions = [
+        { type: REQUEST_BOOK_INFO, ID: "ahsdhl" },
+        { type: RECEIVE_BOOK_INFO, status: "error", payload: "error" },
+      ];
+
+      return store.dispatch(getBookInfo("ahsdhl")).then(() => {
         expect(store.getActions()).toEqual(expectedActions);
       });
     });
